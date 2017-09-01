b0661e015311b4e1d852e1c46d64d9aafc5e245a
m-ferrero <m.ferrero@cognitio.it>
2017-09-01 12:50:11 +0200

    Update plenus-entrypoint.sh

--- plenus-entrypoint.sh
+++ plenus-entrypoint.sh
@@ -3,5 +3,8 @@
 # init or update db
 rake db:migrate
 
+# init SiteSetting.notification_email (only if empty)
+rails r "if SiteSetting.notification_email == '' then SiteSetting.notification_email='$NOTIFICATION_EMAIL' end"
+
 # boot discourse
 /sbin/boot
