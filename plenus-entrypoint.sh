a26c237a3e331fde9553a7d4d92423183a3f1cd0
m-ferrero <m.ferrero@cognitio.it>
2017-09-01 13:06:55 +0200

    Update plenus-entrypoint.sh

--- plenus-entrypoint.sh
+++ plenus-entrypoint.sh
@@ -3,7 +3,7 @@
 # init or update db
 rake db:migrate
 
-# init SiteSetting.notification_email (only if empty)
+# init SiteSetting.notification_email (only if empty or with default value)
 rails r "if SiteSetting.notification_email == '' or SiteSetting.notification_email == 'noreply@unconfigured.discourse.org' then SiteSetting.notification_email='$NOTIFICATION_EMAIL' end"
 
 # boot discourse
