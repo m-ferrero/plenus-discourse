33a30fc8fe6f28e6b94a3355a292fb78626c32fe
m-ferrero <m.ferrero@cognitio.it>
2017-09-08 14:34:37 +0200

    Update plenus-entrypoint.sh

--- plenus-entrypoint.sh
+++ plenus-entrypoint.sh
@@ -3,12 +3,18 @@
 # init or update db
 rake db:migrate
 
-
 # init SiteSetting.notification_email on first startup
 if [ ! -f /shared/state/plenus/notification_email_set ]; then
   rails r "SiteSetting.notification_email='$NOTIFICATION_EMAIL'"
   mkdir -p /shared/state/plenus
   touch /shared/state/plenus/notification_email_set
+fi
+
+# fix permissions on /shared/backups/
+# needed if the directory is used as a mount point for a nested volume
+# because in this case discourse will not create the directory and permissions would be wrong
+if [ -d /shared/backups/ ]; then
+  chown discourse:www-data /shared/backups/
 fi
 
 # boot discourse
